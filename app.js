32740a849f7e821a044b853c1295f5609df04556
jimmyctw <66037440+jimmyctw@users.noreply.github.com>
2021-02-21 05:01:56 +0100

    ecommerce-version2

--- app.js
+++ app.js
@@ -1,21 +1,60 @@
 import { items } from './items.js';
-const resetBtn = document.getElementById('resetBtn');
 const searchCategories = document.getElementsByClassName('search-category');
-const producsContainer = document.getElementById('producsContainer')
-const orderListContainer = document.getElementById('orderListContainer')
+const producsContainer = document.getElementById('producsContainer');
+const orderListContainer = document.getElementById('orderListContainer');
+const orderTitle = document.getElementById('orderTitle');
 const deleteBtns = document.getElementsByClassName('deleteBtn');
 let producsContainerObserver = new MutationObserver( mutationRecords => getCurrentProductsItem())
-// let deleteIteObserver = new MutationObserver( mutationRecords => delete())
+let orderItemObserver = new MutationObserver( mutationRecords => getCurrentOrder())
 let buyItemSet = new Set()
-
-//Confrim Btn & Payment Shows
 const confirmBtn = document.getElementById('confirmBtn');
 const orderPayment = document.getElementById('orderPayment');
 
+//Eventlistener--------------------------------------------------------------------
+    // ----------------- searchCategories-------------------------------- 
+for( let i = 0 ; i < searchCategories.length ; i++ ){
+    const searchCategory = searchCategories[i];
+    searchCategory.addEventListener('click', e => 
+        e.stopPropagation(
+        categoryMatchItem(searchCategory)))
+}
 producsContainerObserver.observe(producsContainer, {
     childList: true,
 })
+orderItemObserver.observe(orderListContainer, {
+    childList: true,
+})
 
+function getCurrentOrder (){
+    confirmBtn.addEventListener('click', ()=> {
+        let orderItems = orderListContainer.getElementsByClassName('order-item');
+        priceCalculator(orderItems)
+        orderPayment.classList.remove('displayNone')
+        confirmBtn.classList.add('displayNone')
+    })
+    for( let i = 0 ; i < deleteBtns.length ; i++ ){
+        const deleteBtn = deleteBtns[i];
+        deleteBtn.addEventListener('click',  ()=>{
+            deleteBtn.parentNode.parentNode.remove()
+        })
+    }
+}
+function priceCalculator(orderItems){
+    let totalPrice = 0;
+    for( let i = 0 ; i < orderItems.length ; i++ ){ 
+        const orderItem = orderItems[i];
+        const orderItemQuantity = parseInt(orderItem.querySelector('.item-quantity').value);
+        const orderItemPrice = Number(orderItem.querySelector('.item-price').innerHTML.replace('$', ''));
+        const orderPrice = (orderItemQuantity * ( orderItemPrice * 10 )) /10
+        totalPrice += orderPrice;
+    }
+    const itemContent = `<span>Thank you very much for your purchase.</span><br><span>The total amount is $ ${totalPrice}</span>`;
+    const itemClass = 'order-item';
+    orderListContainer.innerHTML = '';
+    orderTitle.innerHTML = '';
+    producsContainer.remove();
+    appendItem(itemContent ,itemClass ,orderListContainer)
+}
 
 function getCurrentProductsItem(){
     let buyBtns = Array.from(document.getElementsByClassName('buyBtn'))
@@ -30,33 +69,6 @@ function getCurrentProductsItem(){
         })
     }
 }
-
-
-//Eventlistener--------------------------------------------------------------------
-    // resetBtn.addEventListener('click', reset)
-    // ----------------- searchCategories-------------------------------- 
-    for( let i = 0 ; i < searchCategories.length ; i++ ){
-        const searchCategory = searchCategories[i];
-        searchCategory.addEventListener('click', e => 
-            e.stopPropagation(
-            categoryMatchItem(searchCategory)))
-    }
-
-    // ----------------- CORFIRM Btn-------------------------------- 
-    confirmBtn.addEventListener('click', ()=> {
-        calculateTotalPrice()
-        orderPayment.classList.remove('displayNone')
-    })
-
-
-    for( let i = 0 ; i < deleteBtns.length ; i++ ){
-        const deleteBtn = deleteBtns[i];
-        deleteBtn.addEventListener('click',  ()=>{
-            deleteBtn.parentNode.parentNode.remove()
-        })
-    }
-
-// --------------------- Item Match, Build &  Append  ---------------------
 function categoryMatchItem (searchCategory){
     producsContainer.innerHTML = '';  
     let newItemsList;
@@ -65,12 +77,11 @@ function categoryMatchItem (searchCategory){
             let newSearchCategory = searchCategory.id;
             newItemsList = items.filter(item =>  item.category == newSearchCategory);
         }else{
-            newItemsList = items   
+            newItemsList = items;
         }
     }
     productBuild(newItemsList)
 }
-
 function productBuild (newItemsList){
     for( let i = 0; i < newItemsList.length; i++){
         const id = newItemsList[i].id;
@@ -79,7 +90,7 @@ function productBuild (newItemsList){
         const productClass = 'product-item'
         const itemContent = `
             <div class="item-name">
-                <h1>${name}</h1>
+                <h2>${name}</h2>
             </div>
             <div class="item-img">
                 <img src="/img/item${id}.png" alt="">
@@ -105,56 +116,14 @@ function orderItemBuild(buyItemId){
             <span class="item-price">$ ${price}</span>
         </div>
         <div>
-            <input type="number" min="1" max="10" value="1" class="item-amount" id="amount ${buyItemId}" required>
+            <input type="number" min="1" max="10" value="1" class="item-quantity" id="quantity${buyItemId}" required>
             <button class="deleteBtn"><i class="far fa-trash-alt"></i></button>    
-        </div>
-        `
+        </div>`
     appendItem(orderContent, orderItemClass, orderListContainer)
 }
-
 function appendItem (itemContent, itemClass, container){
     const itemBox = document.createElement('div');
     itemBox.setAttribute("class", itemClass);
     itemBox.innerHTML = itemContent;
     container.appendChild(itemBox)
 }
-
-
-function calculateTotalPrice (){
-    for (let v of buyItemSet.values()){
-        let itemAmount = document.getElementById('amount17').value;
-
-        console.log(itemAmount)
-        console.log(typeof(itemAmount))
-
-    }
-}
-
-
-
-
-
-
-// --------------------- Order Calculator  ---------------------
-// const priceCount = ()=>{
-//     let totalOrderPrice = 0
-//     for (let i = 0; i < itemPrices.length; i++){
-//         const itemPrice = parseInt(itemPrices[i].innerHTML.replace('$', ''));
-//         const itemAmount = parseInt(itemAmounts[i].value);
-//          totalOrderPrice =  totalOrderPrice + (itemPrice * itemAmount);
-//     }
-//     totalPriceAppend(totalOrderPrice)
-// }
-// ---------------------Order List Calculate & Append---------------------
-// const totalPriceAppend = (totalOrderPrice)=>{
-//     const paymentPrice = document.getElementById('paymentPrice');
-//     paymentPrice.innerHTML = '';
-//     const totalPrice = document.createElement('span');
-//     totalPrice.classList.add('span');
-//     totalPrice.innerHTML = `
-//         <button class="countBtn" id="countBtn">COUNT</button>
-//         <span class="totalPrice" id="totalPrice">Total $ ${totalOrderPrice}</span>
-//     `;
-//     paymentPrice.appendChild(totalPrice)
-// }
-
